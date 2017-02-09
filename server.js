fdcc38ea5e06cc734d4f7dab53f52c164840b8ad
rajutheprogrammer <onlyrchandra@gmail.com>
2017-02-09 12:45:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/myget', function (req, res) {
+  res.sendFile("my first get");
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
